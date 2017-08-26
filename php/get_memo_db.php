4e17a2b924b86b58d2da1dba00e2e6a088f02648
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-08-26 15:42:50 +0000

    Show memo including メモ

--- php/get_memo_db.php
+++ php/get_memo_db.php
@@ -6,5 +6,5 @@ $db = new DB();
 
  
 //$db->insert('Insert into memo values (NULL, "ほげ")');
-$memo = $db->select('SELECT id, text FROM memo');
+$memo = $db->select('SELECT id, text FROM memo WHERE text LIKE "%メモ%"');
 Response::send($memo);
